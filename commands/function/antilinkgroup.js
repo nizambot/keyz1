72643edb62bfd9ce26ca69fe8d71201c111cb1d5
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-10-27 16:24:26 +0700

    Update antilinkgroup.js

--- commands/function/antilinkgroup.js
+++ commands/function/antilinkgroup.js
@@ -5,8 +5,9 @@ botAdmin: true,
 
 async functions(m) { 
 let { conn } = data
- if (m.message && m.quoted && m.text.includes("chat.whatsapp.com")) {
+ if (m.message && m.quoted && m.text.includes(`/(https:\/\/chat.whatsapp.com)/gi`) {
  if (!m.sender == this.user.jid) return
+var RegEx = /^[!&z?=#.+\/]/gi;
 m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Group Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
 await conn.groupRemove(m.chat, [m.quoted.sender])
 }
