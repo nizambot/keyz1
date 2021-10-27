d5031fa1095e1009eca33da085866eaa69b394a1
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-10-27 12:26:58 +0700

    Update antivirtex.js

--- commands/function/antivirtex.js
+++ commands/function/antivirtex.js
@@ -5,7 +5,7 @@ botAdmin: true,
 
 async functions(m) { 
 let { conn } = data
- if (m.text && m.text.length >= 7000) {
+ if (m.text && m.text.length >= 10000) {
  if (m.sender == this.user.jid) return false
 m.reply(`Maaf @${m.sender.split("@")[0]} *Virtex Terdeteksi!*\n_Maaf Anda Di Kick._`)
 await conn.groupRemove(m.chat, [m.sender])
