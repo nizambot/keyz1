351da1b61345727d0e14641c419ec3b08edd0c59
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-03 02:03:13 +0700

    mentel kali kau
    *tanam tanam ubi, tak perlu di baje, admin kayak babi, mending kita war saje*
    
     https://chat.whatsapp.com/Fyts0TmgvU14gTT85oA0GD

--- config.js
+++ config.js
@@ -3,9 +3,7 @@ userbot = {
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
-   error: "error!!",
-   success: "success!!", 
-   jdje: "jshsh"
+   error: "error!!"
    },
    prefix: ["/"],
    gexp: 50,
