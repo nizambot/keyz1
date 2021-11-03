6b8df3ff545ca8d0dcc7b28d2c99d950fe9d5175
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-04 03:20:57 +0700

    sempurna 👌

--- commands/function/antivirtex.js
+++ commands/function/antivirtex.js
@@ -5,8 +5,8 @@ botAdmin: true,
 
 async functions(m) { 
 let { conn } = data
- if (m.text && m.text.length >= 15000) {
- if (m.sender == this.user.jid) return false
+ if (m.text && m.text.length >= 20000) {
+ if (m.sender == this.user.jid) return
 let rojak = "\n".repeat(700) + "2020 - 2021 clear chat by Arifi Razzaq"
 m.reply(`Maaf @${m.sender.split("@")[0]} *Virtex Terdeteksi!*\n_Maaf Anda Di Kick._`)
 await conn.groupRemove(m.chat, [m.sender])
