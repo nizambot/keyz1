1d823fa5becf46a1b92306dca10761139b6a0e21
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-03 08:27:20 +0700

    full fix

--- commands/function/antilinkgroup.js
+++ commands/function/antilinkgroup.js
@@ -7,9 +7,12 @@ async functions(m) {
 const gMdata = m.isGroup ? await conn.groupMetadata(m.chat) : '' //Fixed by @arifirazzaq2001
 const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
 const isAdmin = grupAdmin.includes(m.sender)
+const isGroupAdmins = getGroupAdmin.includes(m.sender) || false
+
  if (m.text.includes("https://chat.whatsapp.com/")) { //Fixed by @arifirazzaq2001
  if (m.sender === conn.user.jid) return
  if (m.sender === isAdmin) return
+ if (m.sender === isGroupAdmins) return m.reply("Bot Bukan Admin")
 m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Group Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
 await conn.groupRemove(m.chat, [m.sender])
 }
