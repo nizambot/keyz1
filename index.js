1b145595f77d7619dfe1a5b87c57e594ada0e0f3
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-04 12:53:14 +0700

    Pindah ke ISOXBOT

--- index.js
+++ index.js
@@ -5,65 +5,54 @@ const Baileys = "@adiwajshing/baileys";
 const { WAConnection: _WAConnection } = require("@adiwajshing/baileys");
 const WAConnection = require('./Lib/simple').WAConnection(_WAConnection);
 const { Functions } = require('./Lib/Functions');
-const { JsonDB } =  require("node-json-db")
+const { start } = require('./Lib/banner');
+const { JsonDB } = require("node-json-db")
 const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
-  
 
-global.antidelete = false
-  global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
-    global.Ft = new Functions();// Menghubungkan dari Function.js
-      global.mediaType = require(Baileys).MessageType //Biar keren hehe
-        global.conn = new WAConnection(); //Wa Connect dari baileys
-          global.botuser = require('./config')//Menghubungkan Ke Connection string
-            global.Events = {}
-             global.baileys = Baileys //Hehe
-               global.db = new JsonDB(new Config("database", true, false, '/'));
-                 global.Public = false
-                   global.scrap = require("./Lib/scrape");
+global['write'] = {};
+global['write']['words'] = JSON.parse(fs.readFileSync('./temp/debug.json'));
+global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
+global.Ft = new Functions();// Menghubungkan dari Function.js
+global.mediaType = require(Baileys).MessageType //Biar keren hehe
+global.conn = new WAConnection(); //Wa Connect dari baileys
+global.botuser = require('./config')//Menghubungkan Ke Connection string
+global.Events = {}
+global.baileys = Baileys //Hehe
+global.db = new JsonDB(new Config("database", true, false, '/'));
+global.Public = false
+global.scrap = require("./Lib/scrape");
 
 conn.version = [ 2, 2140, 12 ]
 conn.logger.level = "warn"
-conn.browserDescription = ['R I X L E   B O T', 'SAFARI', '8.1']
+conn.browserDescription = ['ʀɪ𝐱ʟᴇ-ʙᴏᴛ 𝔖Ⅲ', 'SAFARI', '8.1']
 
-if (fs.existsSync('./session.json')) conn.loadAuthInfo('./session.json')
+if (fs.existsSync(global.write.words.qrcode)) conn.loadAuthInfo(global.write.words.qrcode)
 conn.on('qr', qr => {
 qrcode.generate(qr, { small: true })
 console.log(
       conn.logger.warn("[!] Scan Kode QR Diatas, Expired dalam 30 detik")
     )
 })
-conn.on('credentials-updated', () => {
-        conn.logger.warn('credentials updated!')
-        })
-conn.on('connecting', () => {
-        console.log(`Connecting...`)
-        })
+conn.on('credentials-updated', () => {})
+conn.on('connecting', () => {})
 conn.on("open", () => {
-const authInfo = conn.base64EncodedAuthInfo()
-        conn.logger.warn('open docs is complete!')
-        let stats = {
-   status: 200,
-   message: "success",
-   info: "berhasil masuk ke dalam baileys"
-}
-conn.on("ws-close", async() => {
-        conn.logger.warn('Connected Timeout')
-        })
-conn.on("close", async() => {
-        conn.logger.warn('Closed Connection')
-        })
-conn.on("open", () => {
-        conn.longger.warn('reopen docs!')
-        })
+conn.on("ws-close", async() => {})
+conn.on("close", async() => {}) 
 
-console.log(stats)
-fs.writeFileSync('./session.json', JSON.stringify(authInfo, null, '\t'))
+const authInfo = conn.base64EncodedAuthInfo() 
+fs.writeFileSync(global.write.words.qrcode, JSON.stringify(authInfo, null, '\t'))
+
+console.clear()
+conn.logger.warn('Berhasil Perbarui Jaringan\n')
+start('\n',
+    conn.logger.warn('\n')
+  )
 })
 
 setInterval(() => {
- conn.setStatus(`${Ft.count(process.uptime())}`).catch((_) => _)
+ conn.setStatus(`${Ft.count(process.uptime())} | ${Ft.os.hostname()} 𝐑𝐨𝐚𝐝 𝐓𝐨 𝟑𝟎 𝐉𝐮𝐳 🍂 | 𝐏𝐞𝐣𝐮𝐚𝐧𝐠 𝐒𝐡𝐨𝐥𝐚𝐰𝐚𝐭`).catch((_) => _)
 },1000)
- require('./src/loader')
+ require('./src/loader');
 
  async function run() {// Function biar bisa run bot
  let message = require('./action/chats');
@@ -74,5 +63,4 @@ setInterval(() => {
  conn.on('group-participants-update', action.groupUpdate);
  }
  Ft.action()
- console.clear()
  run();// Menjalankan Bot
