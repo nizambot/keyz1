b95ef560a8b80f904d761c732df01c0600601f24
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-05 08:03:30 +0700

    gak bisa duble Explot jika async belum mentok

---
+++ action/battery.js
@@ -0,0 +1,12 @@
+module.exports = {
+async battery(json) {
+let battery = json[2][0][1].value
+let persenbat = parseInt(battery)
+battrey.value = `${persenbat}%`
+battrey.live = json[2][0][1].live
+global.battery = ({
+value: battrey.value,
+isCharge: battry.live
+})
+}
+}
