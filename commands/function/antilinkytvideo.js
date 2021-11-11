6ce7c91a5e76fbb55a5975b1c46945f1e56a9ae8
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-11 08:01:37 +0700

    Update antilinkytvideo.js

--- commands/function/antilinkytvideo.js
+++ commands/function/antilinkytvideo.js
@@ -1,4 +1,4 @@
-/*module.exports = { 
+module.exports = { 
 name: "antilinkytvideo", //By @arifirazzaq2001
 admin: true,
 botAdmin: true,
@@ -31,4 +31,4 @@ function getGroupAdmin(participants) {
 		i.isAdmin ? admins.push(i.jid) : ''
 	}
 	return admins
-} // Full Fix Cok Jangan Di Edit Lagi! By @arifirazzaq2001*/
+} // Full Fix Cok Jangan Di Edit Lagi! By @arifirazzaq2001
